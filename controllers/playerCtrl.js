781257ef5d93459ac9d4e963db3a0a95e6e82192
Squire Johnson <squire.a.johnson@gmail.com>
2015-08-25 12:06:52 -0600

    still buggy

--- controllers/playerCtrl.js
+++ controllers/playerCtrl.js
@@ -8,16 +8,28 @@ module.exports = {
 
 //-- Create New Player
 
+//     newPlayer: function(req, res){
+//       console.log(req.body)
+//       var newPlayer = new Player(req.body);
+//       newPlayer.save(function(err, player) {
+//       if (err) {
+//         console.log(err)
+//         return res.status(500).end();
+//       }
+//         return res.json(player);
+//       });
+//     }
+// }
     newPlayer: function(req, res){
-      console.log(req.body)
-      var newPlayer = new Player(req.body);
-      newPlayer.save(function(err, player) {
-      if (err) {
-        console.log(err)
-        return res.status(500).end();
-      }
-        return res.json(player);
+      Player.create({
+        name: req.body.name,
+        collage: req.body.collage,
+        forty: req.body.forty,
+        benchpress: req.body.benchpress,
+        interestedTeams: req.body.interestedTeams
+      }, function(err, player){
+           if(err) return res.status(500).end();
+           return res.json(player);
       });
     }
 }
-
