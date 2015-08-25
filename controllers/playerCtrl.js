f003a71363e674fe9b476669017e5d21bcb48fa7
Squire Johnson <squire.a.johnson@gmail.com>
2015-08-25 12:13:47 -0600

    working

--- controllers/playerCtrl.js
+++ controllers/playerCtrl.js
@@ -8,18 +8,6 @@ module.exports = {
 
 //-- Create New Player
 
-//     newPlayer: function(req, res){
-//       console.log(req.body)
-//       var newPlayer = new Player(req.body);
-//       newPlayer.save(function(err, player) {
-//       if (err) {
-//         console.log(err)
-//         return res.status(500).end();
-//       }
-//         return res.json(player);
-//       });
-//     }
-// }
     newPlayer: function(req, res){
       Player.create({
         name: req.body.name,
